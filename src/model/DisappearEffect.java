b5d32b9de5ff6231d4254d89236545ae393563c8
Amélie Cordier <acordier@gmail.com>
2016-06-16 22:45:39 +0200

    Version distribuable

---
+++ src/model/DisappearEffect.java
@@ -0,0 +1,21 @@
+/**
+ * MagicP4
+ * IUT Lyon 1 - 2016
+ */
+package model;
+
+/**
+ *
+ * @author hakkahi / acordier - IUT Lyon 1 - 2016
+ */
+public class DisappearEffect extends Effect {
+
+    /**
+     * TODO
+     */
+    @Override
+    public void playEffect(int line, int column, Game game) {
+
+    }
+
+}
